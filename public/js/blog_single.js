15353e33ba2d825dc3f9422981f9b06e6cc4e07d
Cefalu Wave <luke.evan.graham@gmail.com>
2019-10-01 21:47:42 -0700

    working on FB sharing

--- public/js/blog_single.js
+++ public/js/blog_single.js
@@ -12,123 +12,109 @@
 
 ******************************/
 
-$(document).ready(function()
-{
-	"use strict";
+$(document).ready(function() {
+  "use strict";
 
-	/* 
+  /* 
 
 	1. Vars and Inits
 
 	*/
 
-	var header = $('.header');
-	var hamb = $('.hamburger');
-	var menuActive = false;
-	var menu = $('.menu');
+  var header = $(".header");
+  var hamb = $(".hamburger");
+  var menuActive = false;
+  var menu = $(".menu");
 
-	setHeader();
+  setHeader();
 
-	$(window).on('resize', function()
-	{
-		setHeader();
-	});
+  $(window).on("resize", function() {
+    setHeader();
+  });
 
-	$(document).on('scroll', function()
-	{
-		setHeader();
-	});
+  $(document).on("scroll", function() {
+    setHeader();
+  });
 
-	initHeaderSearch();
-	initMenu();
+  initHeaderSearch();
+  initMenu();
 
-	/* 
+  $("#fb_share").click(function(e) {
+    e.preventDefault();
+    FB.ui(
+      {
+        method: "share",
+        href: "https://developers.facebook.com/docs/"
+      },
+      function(response) {}
+    );
+  });
+
+  /* 
 
 	2. Set Header
 
 	*/
 
-	function setHeader()
-	{
-		if($(window).scrollTop() > 100)
-		{
-			header.addClass('scrolled');
-		}
-		else
-		{
-			header.removeClass('scrolled');
-		}
-	}
+  function setHeader() {
+    if ($(window).scrollTop() > 100) {
+      header.addClass("scrolled");
+    } else {
+      header.removeClass("scrolled");
+    }
+  }
 
-	/* 
+  /* 
 
 	3. Init Header Search
 
 	*/
 
-	function initHeaderSearch()
-	{
-		if($('.search_button').length)
-		{
-			$('.search_button').on('click', function()
-			{
-				if($('.header_search_container').length)
-				{
-					$('.header_search_container').toggleClass('active');
-				}
-			});
-		}
-	}
+  function initHeaderSearch() {
+    if ($(".search_button").length) {
+      $(".search_button").on("click", function() {
+        if ($(".header_search_container").length) {
+          $(".header_search_container").toggleClass("active");
+        }
+      });
+    }
+  }
 
-	/* 
+  /* 
 
 	4. Init Menu
 
 	*/
 
-	function initMenu()
-	{
-		if(hamb.length)
-		{
-			if(menu.length)
-			{
-				hamb.on('click', function()
-				{
-					if(menuActive)
-					{
-						closeMenu();
-					}
-					else
-					{
-						openMenu();
-					}
-				});	
+  function initMenu() {
+    if (hamb.length) {
+      if (menu.length) {
+        hamb.on("click", function() {
+          if (menuActive) {
+            closeMenu();
+          } else {
+            openMenu();
+          }
+        });
 
-				$('.menu_close').on('click', function()
-				{
-					if(menuActive)
-					{
-						closeMenu();
-					}
-					else
-					{
-						openMenu();
-					}
-				});
-			}
-		}
-	}
+        $(".menu_close").on("click", function() {
+          if (menuActive) {
+            closeMenu();
+          } else {
+            openMenu();
+          }
+        });
+      }
+    }
+  }
 
-	function closeMenu()
-	{
-		menu.removeClass('active');
-		menuActive = false;
-	}
+  function closeMenu() {
+    menu.removeClass("active");
+    menuActive = false;
+  }
 
-	function openMenu()
-	{
-		menu.addClass('active');
-		menuActive = true;
-	}
-
+  function openMenu() {
+    menu.addClass("active");
+    menuActive = true;
+  }
 });
