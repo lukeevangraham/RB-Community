a95c9b21d71ce7561c121dce9edaab7136d6b027
Cefalu Wave <luke.evan.graham@gmail.com>
2020-02-18 14:46:53 -0800

    further refinement

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -274,7 +274,7 @@ module.exports = function(app) {
         // console.log(vimeoAnnRecord)
 
         // vimeoAnnURL = getIdFromVimeoURL(a);
-        vimeoAnnURL = vimeoAnnRecord.link;
+        vimeoAnnURL = vimeoAnnRecord.data[0].link;
         // console.log("LINK: ", getIdFromVimeoURL(vimeoAnnURL))
       });
 
