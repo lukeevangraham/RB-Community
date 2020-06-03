d0c7b7ec67002323023febb2526d2d6b0ca49922
Cefalu Wave <luke.evan.graham@gmail.com>
2020-06-03 15:54:32 -0700

    preparing for socket io

--- public/js/online-worship.js
+++ public/js/online-worship.js
@@ -1,8 +1,14 @@
-console.log("reporting for duty")
-
 function respondToNavClick() {
-    console.log($(this).attr("data-name"))
+  if ($(this).attr("data-name") === "chat") {
+    $("#sidebarMain").html("<h1>Chat</h1>")
+} else if ($(this).attr("data-name") === "giving") {
+    $("#sidebarMain").html("<h1>Giving</h1>")
+} else if ($(this).attr("data-name") === "prayer") {
+    $("#sidebarMain").html("<h1>Prayer</h1>")
+} else if ($(this).attr("data-name") === "info") {
+    $("#sidebarMain").html("<h1>Info</h1>")
+    
+}
 }
 
-
-$(`.side-nav`).click(respondToNavClick)
+$(`.side-nav`).click(respondToNavClick);
