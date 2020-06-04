aafef7552e67673b61e814f5ba2cb2400b870e5f
Cefalu Wave <luke.evan.graham@gmail.com>
2020-06-03 17:14:10 -0700

    better online chat styling

--- public/js/online-worship.js
+++ public/js/online-worship.js
@@ -1,14 +1,20 @@
 function respondToNavClick() {
   if ($(this).attr("data-name") === "chat") {
-    $("#sidebarMain").html("<h1>Chat</h1>")
-} else if ($(this).attr("data-name") === "giving") {
-    $("#sidebarMain").html("<h1>Giving</h1>")
-} else if ($(this).attr("data-name") === "prayer") {
-    $("#sidebarMain").html("<h1>Prayer</h1>")
-} else if ($(this).attr("data-name") === "info") {
-    $("#sidebarMain").html("<h1>Info</h1>")
-    
+    loadChat();
+  } else if ($(this).attr("data-name") === "giving") {
+    $("#sidebarMain").html("<h1>Giving</h1>");
+  } else if ($(this).attr("data-name") === "prayer") {
+    $("#sidebarMain").html("<h1>Prayer</h1>");
+  } else if ($(this).attr("data-name") === "info") {
+    $("#sidebarMain").html("<h1>Info</h1>");
+  }
 }
+
+function loadChat() {
+  $("#sidebarMain").html(`<ul id="messages"></ul><form id="messageForm" action=""><div class="form-row"><div class="col-10"><input type="text" class="form-control" placeholder="Enter message" id="m"autocomplete="off" /></div><div class="col-2"><div class="input-group-append"><button class="form_submit_button px-2">Send</button></div></div></div></form>`);
 }
+$(document).ready(function () {
+  loadChat();
+});
 
 $(`.side-nav`).click(respondToNavClick);
