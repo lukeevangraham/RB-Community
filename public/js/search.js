58d062cd7954e47a6b5e93b71c247142a04f6c77
Luke Graham <luke.evan.graham@gmail.com>
2022-03-17 17:12:34 -0700

    responsive search

--- public/js/search.js
+++ public/js/search.js
@@ -1,4 +1,15 @@
 const searchSite = () => {
 
-    window.location.replace(`/search:${document.querySelector(".search_input").value}`)
+    if (document.querySelectorAll(".search_input")[0].value) {
+
+        window.location.replace(`/search:${document.querySelectorAll(".search_input")[0].value}`)
+    } else {
+        window.location.replace(`/search:${document.querySelectorAll(".search_input")[1].value}`)
+
+    }
+
+
+
+
+
 }
